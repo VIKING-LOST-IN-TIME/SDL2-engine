a5f84d44f4b69f14154833cac627f7f02628c013
Jeroen <jrnonnekes@gmail.com>
2020-09-16 16:05:24 +0200

    Add project files.

---
+++ SDL2-engine/SDL2-engine.cpp
@@ -0,0 +1,12 @@
+﻿// SDL2-engine.cpp : Defines the entry point for the application.
+//
+
+#include "SDL2-engine.h"
+
+using namespace std;
+
+int main()
+{
+	cout << "Hello CMake." << endl;
+	return 0;
+}
